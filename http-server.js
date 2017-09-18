48e5842a42b3307edef1bbf6df1119c7bdb3f8cb
x373241884y <x373241884y@gmail.com>
2017-09-18 16:22:50 +0800

    https

---
+++ http-server.js
@@ -0,0 +1,11 @@
+// file http-server.js
+var https = require('https');
+var fs = require('fs');
+var options = {
+	key: fs.readFileSync('./keys/server.key'),
+	cert: fs.readFileSync('./keys/server.crt')
+};
+https.createServer(options, function(req, res) {
+	res.writeHead(200);
+	res.end('hello world');
+}).listen(8000);
